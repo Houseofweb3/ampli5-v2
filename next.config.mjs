59910ece940617616140f759f76e47af5d48a6eb
Naimish-Dev <naimishbhesaniya212@gmail.com>
2025-06-01 03:31:06 +0530

    fix:  bounty yaps and images loading issue

--- next.config.mjs
+++ next.config.mjs
@@ -3,24 +3,8 @@ const nextConfig = {
     images: {
       remotePatterns: [
         {
-          protocol: 'https',
-          hostname: 'ui-avatars.com',
-          pathname: '/**', // allows all paths, or you can restrict to specific ones
-        },
-        {
-          protocol: 'https',
-          hostname: 'i.ibb.co',
-          pathname: '/**', // allows all paths, or you can restrict to specific ones
-        },
-        {
-          protocol: 'https',
-          hostname: 'img.youtube.com',
-          pathname: '/**', // allows all paths, or you can restrict to specific ones
-        },
-        {
-          protocol: 'https',
-          hostname: 'pbs.twimg.com',
-          pathname: '/**', // allows all paths, or you can restrict to specific ones
+          protocol: "https",
+          hostname: "**",
         },
       ],
     }
