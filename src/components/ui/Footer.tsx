4434a7ffba04a027179fad4b4560611cb872592f
Naimish-Dev <naimishbhesaniya212@gmail.com>
2026-05-20 22:23:41 +0530

    Update Footer.tsx

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -57,8 +57,19 @@ export default function Footer(): JSX.Element {
             </div>
           </div>
           <div className="flex  flex-col sm:flex-row gap-5 sm:gap-7 justify-between items-center">
-            <div className="text-14 sm:text-18 font-medium">
-              @{new Date().getFullYear()} HOW3 PTE LTD. All rights reserved.
+            <div className="text-14 sm:text-18 font-medium text-center sm:text-left">
+              @{new Date().getFullYear()} HOW3 PTE LTD. All rights reserved.{" "}
+              <span className="text-black/70">
+                Crafted by{" "}
+                <Link
+                  href="https://www.bycode.in/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-black hover:text-primary transition-colors duration-200"
+                >
+                  ByCode
+                </Link>
+              </span>
             </div>
             <div className="flex items-center gap-4">
               <Link target="_blank" href="https://x.com/nomo_nomaya">
